b79035d7a6d015656c03f98dc1ba39a7072ca491
nguyenkhanhtung <tungnk0711@gmail.com>
2018-08-06 17:41:46 +0700

    first commit project

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,20 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double weight;
+        double height;
+        double bmi;
+
+        System.out.println("Enter your weight (kilogram): ");
+        weight = scanner.nextDouble();
+
+        System.out.println("Enter your height (meter): ");
+        height = scanner.nextDouble();
+
+    }
+}
